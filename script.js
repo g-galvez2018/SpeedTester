37be4968151ef82ab89d16b53ee72066ceafb049
Gustavo Galvez <gustavo.galvez09@gmail.com>
2020-04-29 01:28:46 -0400

    Added timer function

--- script.js
+++ script.js
@@ -4,22 +4,52 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+const timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+  if (time <= 9) {
+    time = '0' + time;
+  }
+  return time;
+}
 
 // Run a standard minute/second/hundredths timer:
+function runTimer () {
+  let currentTime = `${leadingZero(timer[0])}:${leadingZero(timer[1])}:${leadingZero(timer[2])}`
+  console.log (currentTime);
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
 
+  timer[0] = Math.floor((timer[3]/100)/60);
+  timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+  timer[2] = Math.floor((timer[3]) - (timer[1] * 100) - (timer[0]* 6000));
+  
+}
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
   let textEntered = testArea.value;
-  console.log(textEntered)
+  let originTextMatch = originText.substring(0,textEntered.length)
+
+  if (textEntered == originText) {
+    testWrapper.style.borderColor = "#429890";
+  } else {
+    if (textEntered == originTextMatch) {
+      testWrapper.style.borderColor = "#65CCf3";
+    } else {
+      testWrapper.style.borderColor = "#E95D0F";
+    }
+  }
+  
 }
 
 // Start the timer:
 function start() {
   let textEnteredLength = testArea.value.length;
+  if (textEnteredLength === 0) {
+    setInterval(runTimer,10);
+  }
   console.log(textEnteredLength);
 }
 
